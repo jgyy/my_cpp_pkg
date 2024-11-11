0d7b3a6120b0c41188d9712b3a75f1d8b43b5586
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-11 20:06:02 +0800

    added cpp param

--- src/number_publisher.cpp
+++ src/number_publisher.cpp
@@ -6,8 +6,10 @@ class NumberPublisher : public rclcpp::Node
 public:
     NumberPublisher() : Node("number_publisher"), number_(2)
     {
-        publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
+        this->declare_parameter("name");
 
+        publisher_ = this->create_publisher<example_interfaces::msg::Int64>(
+                                                                        "number", 10);
         timer_ = this->create_wall_timer(
             std::chrono::seconds(1),
             std::bind(&NumberPublisher::publish_number, this));
