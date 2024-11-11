f66d8c581eba34c0b5967e18b9b5c425f8e7051c
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-11 20:37:03 +0800

    add param for cpp

--- src/number_publisher.cpp
+++ src/number_publisher.cpp
@@ -4,14 +4,19 @@
 class NumberPublisher : public rclcpp::Node
 {
 public:
-    NumberPublisher() : Node("number_publisher"), number_(2)
+    NumberPublisher() : Node("number_publisher")
     {
-        this->declare_parameter("name");
+        this->declare_parameter("number_to_publish", 2);
+        this->declare_parameter("publish_frequency", 1.0);
+
+        number_ = this->get_parameter("number_to_publish").as_int();
+        double publish_frequency = this->get_parameter(
+                                                    "publish_frequency").as_double();
 
         publisher_ = this->create_publisher<example_interfaces::msg::Int64>(
                                                                         "number", 10);
         timer_ = this->create_wall_timer(
-            std::chrono::seconds(1),
+            std::chrono::milliseconds((int) (1000.0 / publish_frequency)),
             std::bind(&NumberPublisher::publish_number, this));
 
         RCLCPP_INFO(this->get_logger(), "Number Publisher has been started");
@@ -27,7 +32,7 @@ private:
 
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
-    const int64_t number_;
+    int number_;
 };
 
 int main(int argc, char **argv)
